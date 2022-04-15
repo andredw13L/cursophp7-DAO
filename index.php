9bcd601134db0935ec6929244094734629374783
andredw13L <andreribeiro13l@gmail.com>
2022-04-15 00:59:51 -0300

    Métodos de lista

--- index.php
+++ index.php
@@ -4,10 +4,24 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once("config.php");
 
-$jose = new Usuario();
+//Carregar um usuário
+//$jose = new Usuario();
+//$jose->loadbyId(3);
+//echo $jose;
 
-$jose->loadbyId(3);
+//Carrega uma lista de usuário 
+//$lista = Usuario::getList(); 
+//echo json_encode($lista);
 
-echo $jose;
+// Carrega uma lista de usuários buscando pelo nome
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha 
+
+$usuario = new Usuario();
+$usuario->login("root","!@#$");
+
+echo $usuario;
 
 ?>
